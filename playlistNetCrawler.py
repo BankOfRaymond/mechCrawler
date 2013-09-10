bd66e76f2dcfc3e425264abeaa9d7fa8ad23bc77
cyrano821 <cyrano821@mossberg>
2013-09-10 15:51:04 -0700

    PlaylistNet crawler starting

--- playlistNetCrawler.py
+++ playlistNetCrawler.py
@@ -22,7 +22,8 @@ AGENT_ALIASES = {
     'Android' : 'Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13'
 }
 
-
+genres = ['alternative','blues','classical','comedy','compilation','country','dance','disco','electronica','folk','heavy-metal','hip-hoprap','house','jazz','latin','pop','punk','rb','reggae','rock','soul','soundtrack','techno','world']
+moods = [ 'angry','chillout','cool','dark','dramatic','energetic','funny','futuristic','groovy','happy','intimate','party']
 
 #print AGENT_ALIASES['Windows IE 9']
 
@@ -69,3 +70,6 @@ br.submit()
 time.sleep(1)
 br.open('http://playlists.net/new-releases')
 print br.response().read()
+
+
+class PlaylistNetCrawler
