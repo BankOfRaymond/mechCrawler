9aa11c3514a92697164c2f4da4b2e0635396e823
Raymond Walintukan <raytochina@gmail.com>
2013-09-14 16:58:00 -0700

    testing added

---
+++ Testing.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 14, 2013
+
+@author: cyrano821
+'''
+
+class Testing():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        True
+        print True
+        
+t = Testing()
